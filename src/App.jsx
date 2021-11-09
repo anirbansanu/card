dfd15ae21ff433e4ee68399ef9e85f470e40d2b8
Anirban <anirbanmukherjee602@gmail.com>
2021-11-09 11:31:39 +0530

    ani-bg-transparent added

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,22 @@ function App() {
       <div className="container-fluid p-4 mb-5">
           <div className="row m-0">
             <div className="col-12 col-md-6 col-lg-4 mt-5">
-              <AniCard headerColor="white" expand={true} height="200px"/>
+              <AniCard allTools={true} headerColor="white" expand={true} expandIcon='expand_more' collapseIcon="expand_less" height="200px"/>
             </div>
-            <div className="col-12 col-md-6 col-lg-4 mt-5" >
-              <AniCard headerColor="success" expand={false} height="200px"/>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5" >
+              <AniCard allTools={true} headerColor="success" expand={false} height="200px"/>
             </div>
-            <div className="col-12 col-md-6 col-lg-4 mt-5">
-              <AniCard headerColor="purple" expand={true} height="200px"/>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5">
+              <AniCard expandBtn={true} transparent='ani-bg-transparent-txt-black' expand={true} height="200px"/>
             </div>
-            <div className="col-12 col-md-6 col-lg-4 mt-5">
-              <AniCard headerColor="danger" expand={true} height="200px"/>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5">
+              <AniCard maximizeBtn={true} headerColor="danger" expand={true} height="200px"/>
             </div>
-            <div className="col-12 col-md-6 col-lg-4 mt-5">
-              <AniCard headerColor="warning" expand={true} height="200px"/>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5">
+              <AniCard closeBtn={true} headerColor="warning" expand={true} height="200px"/>
             </div>
-            <div className="col-12 col-md-6 col-lg-4 mt-5">
-              <AniCard headerColor="alert" expand={true} height="200px"/>
+            <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5">
+              <AniCard maximizeBtn={true} expandBtn={true} closeBtn={true} headerColor="alert" expand={true} height="200px"/>
             </div>
           </div>
       </div>
