f02c7d62bcc9358ed7168d5a74c7d428b367229d
Anirban <anirbanmukherjee602@gmail.com>
2021-11-09 11:32:48 +0530

    just update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AniCard from './components/AniCard/AniCard';
 function App() {
   return (
     <>
-      <div className="container-fluid p-4 mb-5">
+      <div className="container-fluid p-4 mb-5 bg-dark">
           <div className="row m-0">
             <div className="col-12 col-md-6 col-lg-4 mt-5">
               <AniCard allTools={true} headerColor="white" expand={true} expandIcon='expand_more' collapseIcon="expand_less" height="200px"/>
@@ -14,7 +14,7 @@ function App() {
               <AniCard allTools={true} headerColor="success" expand={false} height="200px"/>
             </div>
             <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5">
-              <AniCard expandBtn={true} transparent='ani-bg-transparent-txt-black' expand={true} height="200px"/>
+              <AniCard expandBtn={true} transparent='ani-bg-transparent-txt-white' expand={true} height="200px"/>
             </div>
             <div className="col-12 col-sm-12 col-md-6 col-lg-4 mt-5">
               <AniCard maximizeBtn={true} headerColor="danger" expand={true} height="200px"/>
